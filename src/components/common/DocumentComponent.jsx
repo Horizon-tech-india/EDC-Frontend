c77004dd2910018b8d785a28b826397386b111a5
Sumit063 <aspp775@gmail.com>
2023-04-25 20:50:38 +0530

    updated documents page

--- src/components/common/DocumentComponent.jsx
+++ src/components/common/DocumentComponent.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "../../styles/documentsWaitComponent.css"
+import { DownloadBtn, OutlinedBtn, Btn } from './Buttons';
+import { NavLink } from 'react-router-dom';
+
 
 const DocumentComponent = () => {
   const headingWaitSec = "Waiting for the approval";
@@ -8,13 +11,37 @@ const DocumentComponent = () => {
 
     const textBodyWaitSec2 = "Admin PPDB SMPN 1 Cibadak";
 
+    const DownloadQnA = () => {};
+    const DownloadPageFormat = () => {};
+    const handleSizeDetails = () => {};
+    const handleGoHome = () => {
+        
+    };
+
+
   return (
     // <div className="min-h-screen w-full flex justify-center items-center">
     //   DocumentComponent
     // </div>
 
     <div className="waiting-sec-container">
+         <div className="btns-mid-col">
+         <NavLink
+            to="/"
+          ><Btn btntext="Go Home" />
+          </NavLink>
+            </div>
     <div className="waiting-sec-content-container">
+        <div className="documents-btn-groups">
+            <div className="btns-left-col">
+                <DownloadBtn btntext="Q&A"/>
+                <DownloadBtn btntext="Download the proper format"/>
+            </div>
+            <div className="btns-right-col">
+                <OutlinedBtn btntext="click here for size details" />
+            </div>
+            
+        </div>
         <div className="waiting-sec-content">
             <div className="waiting-sec-content-row1">
                 <h1> { headingWaitSec } </h1>
