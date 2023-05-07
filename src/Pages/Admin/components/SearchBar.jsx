7654e08412959a95567c7c32b3ee6d9f41aa5d15
Jayant Rohila <jrohila55@gmail.com>
2023-05-07 11:53:35 +0530

    updated search bar

--- src/Pages/Admin/components/SearchBar.jsx
+++ src/Pages/Admin/components/SearchBar.jsx
@@ -2,19 +2,19 @@ import searchIcon from '../../../assets/search-normal.svg'
 import React, { useState, useEffect, useContext } from 'react'
 import { AuthContext } from '../../../context/AuthContext'
 import { API } from '../../../Api/Post'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import { Divider } from '@mui/material';
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+// import div from '@mui/material/div'
+import Modal from '@mui/material/Modal'
+// import { Divider } from '@mui/material'
 
 function SearchBar() {
   const [allStartups, setAllStartups] = useState([])
   const [filteredData, setFilteredData] = useState([])
   const [inputValue, setInputValue] = useState('')
   const [open, setOpen] = useState(true)
-  const [modalState, setModalState]=useState(false)
-  const [userData,setUserData]=useState({
+  const [modalState, setModalState] = useState(false)
+  const [userData, setUserData] = useState({
     aadhar: '',
     branch: '',
     category: '',
@@ -37,16 +37,32 @@ function SearchBar() {
     title: '',
     uniqueFeatures: '',
     updatedAt: '',
-
-
   })
   const { state } = useContext(AuthContext)
+  let debounceTimeout
 
+  const debounce = (callback, delay) => {
+    clearTimeout(debounceTimeout)
+    debounceTimeout = setTimeout(callback, delay)
+  }
   useEffect(() => {
     API('get', '/admin/all-startup-details', {}, state.token)
       // .then((data) => console.log(`SEARCH BAR FILTER`, data?.data?.data))
       .then((data) => setAllStartups(data?.data?.data))
       .catch((error) => console.error(error))
+  }, [])
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.closest('.modal')) {
+        return
+      }
+      setOpen(true)
+      setInputValue('')
+    }
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
   }, [])
 
   useEffect(() => {
@@ -59,138 +75,212 @@ function SearchBar() {
   }, [allStartups, inputValue])
 
   const handleInputChange = (event) => {
-    console.log(filteredData)
-    setInputValue(event.target.value)
     const value = event.target.value.trim()
-    if (value.length > 0) {
-      setOpen(false)
-    } else {
-      setOpen(true)
-    }
+    debounce(() => {
+      setInputValue(value)
+      if (value.length > 0) {
+        setOpen(false)
+      } else {
+        setOpen(true)
+      }
+    }, 200)
   }
-  
-  
 
   const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 800,
     bgcolor: 'background.paper',
     boxShadow: '24px',
+    borderRadius: '10px',
     p: 4,
-  };
-  const handleClick=(data)=>{
+  }
+  const handleClick = (data) => {
     setModalState(true)
 
-setUserData((prev)=>{
-  return  {
-  ...prev,
-  aadhar:data.aadhar,
-    branch: data.branch,
-    category: data.category,    
-    categoryOther: data.categoryOther,
-    contact: data.contact,    
-    currentStage: data.currentStage,    
-    designation:data.designation,    
-    email: data.email,
-    enrollmentNum:data.enrollmentNum,  
-    institute: data.institute,
-    location: data.location,
-    name: data.name,
-    otherInstitute: data.otherInstitute,
-    otherOrganisation: data.otherOrganisation,
-    otherUniversity: data.otherUniversity,
-    startupId: data.startupId,
-    status: data.status,
-    teamMembers: data.teamMembers,
-    teamSize: data.teamSize,
-    title: data.title,
-    uniqueFeatures: data.uniqueFeatures,
-    updatedAt: data.updatedAt
+    setUserData((prev) => {
+      return {
+        ...prev,
+        aadhar: data.aadhar,
+        branch: data.branch,
+        category: data.category,
+        categoryOther: data.categoryOther,
+        contact: data.contact,
+        currentStage: data.currentStage,
+        designation: data.designation,
+        email: data.email,
+        enrollmentNum: data.enrollmentNum,
+        institute: data.institute,
+        location: data.location,
+        name: data.name,
+        otherInstitute: data.otherInstitute,
+        otherOrganisation: data.otherOrganisation,
+        otherUniversity: data.otherUniversity,
+        startupId: data.startupId,
+        status: data.status,
+        teamMembers: data.teamMembers,
+        teamSize: data.teamSize,
+        title: data.title,
+        uniqueFeatures: data.uniqueFeatures,
+        updatedAt: data.updatedAt,
+      }
+    })
   }
-})
 
-  }
-
-  const handleClose=()=>{
-  setModalState(false)
+  const handleClose = () => {
+    setModalState(false)
   }
   return (
     <>
-    <div className=" search-bar w-full">
-      <div className="search-wrapper">
-        <img src={searchIcon} alt="searchbar" className="search-icon" />
-        <input
-          className="search-input  "
-          type="text"
-          placeholder="Search startups"
-          onChange={handleInputChange}
-          value={inputValue}
-        />
+      <div className=" search-bar w-full">
+        <div className="search-wrapper   focus:border-2 border-[#b4cd93] ">
+          <img src={searchIcon} alt="searchbar" className="search-icon" />
+          <input
+            className="search-input focus:border-2 border-[#b4cd93]"
+            type="text"
+            placeholder="Search startups"
+            onChange={handleInputChange}
+            value={inputValue}
+          />
+        </div>
+
+        <div
+          className={`fixed ${
+            open ? 'hidden' : 'block'
+          }  h-auto w-full max-w-2xl shadow-2xl shadow-black  top-16 z-50 p-2 bg-[#fafafa] border border-gray-200  rounded-md`}
+          id="filter-results"
+        >
+          <ul className="grid grid-cols-10 px-1 cursor-pointer">
+            <li className="col-span-3"> Company Name</li>
+            <li className="col-span-2"> Name</li>
+            <li className="col-span-3">Email</li>
+            <li className="col-span-2">Branch</li>
+          </ul>
+          {filteredData.map((item) => (
+            <div
+              className="bg-[#e5e5e5] modal hover:text-white hover:bg-[#b4cd93]  text-xs capitalize font-light hover:font-normal border border-gray-200 p-2 w-full rounded-md my-1"
+              key={item.startupId}
+              onClick={() => handleClick(item)}
+            >
+              <ul className="grid grid-cols-10 cursor-pointer">
+                <li className="col-span-3"> {item.title}</li>
+                <li className="col-span-2"> {item.name}</li>
+                <li className="col-span-3"> {item.email}</li>
+                <li className="col-span-2"> {item.branch}</li>
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
 
-      <div
-        className={`fixed ${
-          open ? 'hidden' : 'block'
-        }  h-auto w-full max-w-3xl top-16 z-50 p-2 bg-[#fafafa] border border-gray-200  rounded-md`}
-        id="filter-results"
-      >
-        {filteredData.map((item) => (
-          <div
-            className="bg-white text-xs capitalize font-light border border-gray-200 p-2 w-full rounded-md my-1"
-            key={item.startupId}
-            onClick={()=>handleClick(item)}
-          >
-            <ul className="grid grid-cols-12 cursor-pointer" >
-              <li className='col-span-2'> {item.name}</li>
-              <li className='col-span-3'> {item.email}</li>
-              <li className='col-span-2'> {item.location}</li>
-              <li className='col-span-1'> {item.branch}</li>
-              <li className='col-span-2'> {item.startupId}</li>
-              <li className='col-span-2'> {item.title}</li>
-            </ul>
-          </div>
-        ))}
+      <div>
+        <Modal
+          open={modalState}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div className="text-xl py-2 w-full text-center">Company Details</div>
+            <div className="grid grid-cols-2	">
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs">Company Name </span> :{' '}
+                <span className="text-xs">{userData.title} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Name </span> :{' '}
+                <span className="text-xs">{userData.name} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Branch </span> :{' '}
+                <span className="text-xs">{userData.branch} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Category </span> :{' '}
+                <span className="text-xs"> {userData.category} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Email </span> :{' '}
+                <span className="text-xs"> {userData.email} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Category Other </span> :{' '}
+                <span className="text-xs"> {userData.categoryOther} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Contact </span> :{' '}
+                <span className="text-xs"> {userData.contact} </span>
+              </div>
+
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Designation </span> :{' '}
+                <span className="text-xs"> {userData.designation} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Enrollment Num </span> :{' '}
+                <span className="text-xs"> {userData.enrollmentNum} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Institute </span> :{' '}
+                <span className="text-xs"> {userData.institute} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Location </span> :{' '}
+                <span className="text-xs"> {userData.location} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Other Institute </span> :{' '}
+                <span className="text-xs"> {userData.otherInstitute} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Other Organisation </span> :{' '}
+                <span className="text-xs"> {userData.otherOrganisation} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Other University </span> :{' '}
+                <span className="text-xs"> {userData.otherUniversity} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Startup Id </span> :{' '}
+                <span className="text-xs"> {userData.startupId} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Status </span> :{' '}
+                <span className="text-xs"> {userData.status} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Team Members </span> :{' '}
+                <span className="text-xs"> {userData.teamMembers} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Team Size </span> :{' '}
+                <span className="text-xs"> {userData.teamSize} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> Unique Features </span> :{' '}
+                <span className="text-xs"> {userData.uniqueFeatures} </span>
+              </div>
+              <div className="justify-items-center">
+                <span className="capitalize text-green-600 m-3 text-xs"> updated at </span> :{' '}
+                <span className="text-xs"> {userData.updatedAt} </span>
+              </div>
+              <div className="justify-items-center col-span-2 ">
+                <span className="capitalize text-green-600 m-3 text-xs"> Current Stage </span> :{' '}
+                <span className="text-xs max-w-xs  "> {userData.currentStage} </span>
+              </div>
+            </div>
+            <div className="w-full flex my-2 justify-center items-center">
+              <Button onClick={handleClose} size="sm" variant="contained" color="success">
+                Close
+              </Button>
+            </div>
+          </Box>
+        </Modal>
       </div>
-    </div>
-    
-    <div>
-      
-      <Modal
-        open={modalState}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-        
-          <Typography id="modal-modal-title justify-items-center"  classes='text-xs'>
-          Company Details
-          </Typography>
-          <Typography id="modal-modal-title justify-items-center" variant="p" component="p">
-          <span  className='capitalize text-green-600 m-3 text-xs'> Name </span> : <span className='text-xs'>{userData.name} </span>
-          </Typography>
-          <Divider />
-          <Typography id="modal-modal-title justify-items-center" variant="p" component="p">
-          <span  className='capitalize text-green-600 m-3 text-xs'> Branch </span> : <span className='text-xs'>{userData.branch} </span>
-          </Typography>
-          <Divider />
-          <Typography id="modal-modal-title justify-items-center" variant="p" component="p">
-          <span  className='capitalize text-green-600 m-3 text-xs'> Email   </span> : <span className='text-xs'> {userData.email} </span>
-          </Typography>
-          <Divider />
-                    
-          <Button onClick={handleClose} size='sm' variant='contained' color='success'>Close</Button>
-        </Box>
-      </Modal>
-    </div>
-    
-            
-            </>
+    </>
   )
 }
-
 
 export default SearchBar
